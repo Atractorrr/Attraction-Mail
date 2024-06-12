f708ca507bf052762f9c6644ee6b9a6a1e689106
uhanuu <hwryu990927@gmail.com>
2024-06-12 10:18:09 +0900

    refactor: wrapper 객체를 통해서 image 라이브러리 의존성 제거

--- src/main/kotlin/attraction/run/html/ThumbnailUrlParser.kt
+++ src/main/kotlin/attraction/run/html/ThumbnailUrlParser.kt
@@ -1,17 +1,16 @@
 package attraction.run.html
 
 import attraction.run.s3.S3Service
-import com.sksamuel.scrimage.ImmutableImage
 import com.sksamuel.scrimage.webp.WebpWriter
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Service
-import java.awt.image.BufferedImage
+import org.springframework.stereotype.Component
 import java.io.File
 import java.net.URI
 import java.util.*
 import javax.imageio.ImageIO
 
-@Service
+
+@Component
 class ThumbnailUrlParser(
         @Value("\${file.thumbnail-path}")
         private val path: String,
@@ -19,16 +18,14 @@ class ThumbnailUrlParser(
 ) {
     private companion object {
         private val EXTENSIONS = listOf(".jpg", ".jpeg", ".png")
-        private val IMAGE_ASPECT_RATIO_RANGE = 56.25..100.00
+        private val IMAGE_ASPECT_RATIO_RANGE = 30.00..100.00
         private const val TARGET_WIDTH = 720
         private const val WEBP_SUFFIX = ".webp"
     }
-
     fun getThumbnailUrl(thumbnailUrls: List<String>): String {
         initFilePath()
         val bufferImages = getBufferImages(thumbnailUrls)
         val images = bufferImages.filter(::imageRule).take(2)
-
         return when (images.size) {
             1 -> imageResizeAndConvertWebp(images[0])
             2 -> imageResizeAndConvertWebp(images[1])
@@ -43,32 +40,26 @@ class ThumbnailUrlParser(
         }
     }
 
-    private fun getBufferImages(thumbnailUrls: List<String>): List<BufferedImage> {
+    private fun getBufferImages(thumbnailUrls: List<String>): List<ThumbnailImg> {
         return thumbnailUrls.filter {
             EXTENSIONS.any { it.endsWith(it, ignoreCase = true) }
         }.map {
-            ImageIO.read(URI(it).toURL())
+            ThumbnailImg(ImageIO.read(URI(it).toURL()))
         }
     }
 
-    private fun imageRule(it: BufferedImage) =
-            it.height >= 300 && (it.height.toDouble() / it.width) * 100 in IMAGE_ASPECT_RATIO_RANGE
+    private fun imageRule(it: ThumbnailImg) =
+            it.hasMinimumImageSize(300) && it.hasAspectRatioRange(IMAGE_ASPECT_RATIO_RANGE)
 
-    private fun imageResizeAndConvertWebp(image: BufferedImage): String {
-        val immutableImage = getImmutableImage(image)
-                .scaleToWidth(TARGET_WIDTH)
-                .output(WebpWriter.DEFAULT, File("$path/${UUID.randomUUID()}$WEBP_SUFFIX"))
+    private fun imageResizeAndConvertWebp(thumbnailImg: ThumbnailImg): String {
+        val convertImage = if (thumbnailImg.isImageSizeSmallerWidth(TARGET_WIDTH)) {
+            thumbnailImg.getImmutableImage()
+        } else {
+            thumbnailImg.getImmutableImageWithScaleToWidth(TARGET_WIDTH)
+        }.output(WebpWriter.DEFAULT, File("$path/${UUID.randomUUID()}$WEBP_SUFFIX"))
 
-        s3Service.uploadThumbnailImg(immutableImage)
-        immutableImage.delete()
-        return immutableImage.name
+        s3Service.uploadThumbnailImg(convertImage)
+        convertImage.delete()
+        return convertImage.name
     }
-
-    private fun getImmutableImage(image: BufferedImage): ImmutableImage =
-            ImmutableImage.create(
-                    image.width,
-                    image.height,
-                    PixelFactory.getPixelArrayFromImage(image),
-                    BufferedImage.TYPE_3BYTE_BGR
-            )
 }
